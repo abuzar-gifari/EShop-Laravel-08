2093423f112df2b6ece4cb4cb3c9a365f0851527
abuzar-gifari <shaikh35-3024@diu.edu.bd>
2022-04-29 23:10:08 +0600

    admin work 80% done

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,10 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
+Route::get('/',function(){
+    return view('welcome');
+});
+
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::middleware(['auth','isAdmin'])->group(function(){
