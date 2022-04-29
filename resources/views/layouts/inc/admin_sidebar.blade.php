88acd2c3d6161e06e6e08432bce0664870746a63
abuzar-gifari <shaikh35-3024@diu.edu.bd>
2022-04-29 15:50:39 +0600

    make logout button

--- resources/views/layouts/inc/admin_sidebar.blade.php
+++ resources/views/layouts/inc/admin_sidebar.blade.php
@@ -33,12 +33,14 @@
         </a>
       </li>
       <li class="nav-item">
-        <a class="nav-link text-white " href="./pages/sign-in.html">
-          <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
-            <i class="material-icons opacity-10">login</i>
-          </div>
-          <span class="nav-link-text ms-1">Sign In</span>
+        <a class="dropdown-item" href="{{ route('logout') }}" 
+        onclick="event.preventDefault();
+                      document.getElementById('logout-form').submit();">
+            {{ __('Logout') }}
         </a>
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+            @csrf
+        </form>
       </li>
     </ul>
   </div>
